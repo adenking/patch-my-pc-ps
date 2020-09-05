fc6f57930b92382e6eebe8e9d25136339877a906
Aden King <adenking@live.com>
2020-09-05 20:34:49 +1000

    make it run the exe and gitignore the files

--- update.ps1
+++ update.ps1
@@ -5,3 +5,4 @@ $start_time = Get-Date
 Invoke-WebRequest -Uri $url -OutFile $output
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
+Start-Process -FilePath "$PSScriptRoot\PatchMyPC.exe"
