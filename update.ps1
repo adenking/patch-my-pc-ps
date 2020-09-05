870ad112f22e5ab69bac9a600edf030871826fa2
Aden King <adenking@live.com>
2020-09-05 20:32:20 +1000

    initial commit

---
+++ update.ps1
@@ -0,0 +1,7 @@
+$url = "https://patchmypc.com/freeupdater/PatchMyPC.exe"
+$output = "$PSScriptRoot\PatchMyPC.exe"
+$start_time = Get-Date
+
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
